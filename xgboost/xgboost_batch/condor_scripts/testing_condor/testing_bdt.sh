3502a7a876944b1e563011d55cf7d20fb7f9a2f0
Thomas Matthew Critchley <tcritchl@lxplus710.cern.ch>
2024-02-28 14:26:22 +0100

    results run 6

--- xgboost/xgboost_batch/condor_scripts/testing_condor/testing_bdt.sh
+++ xgboost/xgboost_batch/condor_scripts/testing_condor/testing_bdt.sh
@@ -2,5 +2,5 @@
 
 source /cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc11-opt/setup.sh
 
-python3 /afs/cern.ch/user/t/tcritchl/xgboost_batch/testing_macro.py
+python3 /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/xgboost/xgboost_batch/testing_macro.py
 
