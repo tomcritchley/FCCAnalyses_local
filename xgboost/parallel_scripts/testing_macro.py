2ae599d90e1a1488086513779659eb2ea2dfbcc2
Thomas <thomas.critchley@etu.unige.ch>
2024-03-11 23:31:47 +0100

    json results management

--- xgboost/parallel_scripts/testing_macro.py
+++ xgboost/parallel_scripts/testing_macro.py
@@ -8,6 +8,7 @@ import os
 import argparse
 from sklearn.metrics import roc_curve, auc
 import json
+import fcntl
 
 ##################################################################################################################
 ###################################### INPUTS ####################################################################
@@ -337,8 +338,21 @@ if __name__ == "__main__":
         print(f"An error occurred: {e}")
 
     json_file_path = f"/afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/xgboost/xgboost_batch/test_xgboost_results{run}_10fb.json"
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as json_file:
+            existing_results = json.load(json_file)
+    else:
+        existing_results = {}
+
+    lockfile = open(json_file_path, 'a')
+    fcntl.flock(lockfile, fcntl.LOCK_EX)
+
+    existing_results.update(results_dict)
+
+    fcntl.flock(lockfile, fcntl.LOCK_UN)
+    lockfile.close()
 
     with open(json_file_path, "w") as json_file:
-        json.dump(results_dict, json_file, indent=2)
+        json.dump(existing_results, json_file, indent=2)
 
     print(f"Results saved to {json_file_path}")
