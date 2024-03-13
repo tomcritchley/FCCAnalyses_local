9e8436e21d0af4bdd6ae68232dcb102a44c6afa4
Thomas Matthew Critchley <tcritchl@lxplus919.cern.ch>
2024-03-13 11:47:25 +0100

    organising

--- Analysis_cut_and_count/analysis_final.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /afs/cern.ch/work/t/tcritchl/FCCAnalyses/setup.sh
-
-fccanalysis final /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/Analysis_cut_and_count/analysis_final_2.py
