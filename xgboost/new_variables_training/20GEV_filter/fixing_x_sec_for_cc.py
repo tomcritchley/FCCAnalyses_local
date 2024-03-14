6719169c1ae9194ecbf95719174f42a8112dbe18
Thomas <thomas.critchley@etu.unige.ch>
2024-03-14 12:23:11 +0100

    changes

--- xgboost/new_variables_training/20GEV_filter/fixing_x_sec_for_cc.py
+++ xgboost/new_variables_training/20GEV_filter/fixing_x_sec_for_cc.py
@@ -1,5 +1,6 @@
 import uproot
 import os
+import awkward as ak
 
 # Define the directory containing the root files
 root_dir = "/eos/user/t/tcritchl/xgBOOST/fullstats/withvertex/p8_ee_Zcc_ecm91/"
@@ -15,10 +16,15 @@ def update_cross_section(root_file):
             # Get the events tree
             tree = file["events"]
             # Update the cross section value
-            tree["cross_section"].array()[:] = new_cross_section
+            cross_section_array = tree["cross_section"].array()
+            print(f"cross section array: {cross_section_array}")
+            updated_cross_section_array = ak.mask(cross_section_array, cross_section_array == 6654.46, new_cross_section)
+            print(f"updated cross section array: {updated_cross_section_array}")
+            tree["cross_section"].array()[:] = updated_cross_section_array
             # Save the changes
             file["events"] = tree
             print(f"Cross section updated in {root_file}")
+            root_file.close()
     except Exception as e:
         print(f"Error updating cross section in {root_file}: {e}")
 
