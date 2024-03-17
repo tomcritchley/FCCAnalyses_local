f68e9e42030fef79a55c5d8455e07da64b8bf5db
Thomas Matthew Critchley <tcritchl@lxplus731.cern.ch>
2024-03-17 14:00:43 +0100

    changes

--- xgboost/new_variables_training/20GEV_filter/RunAnSt1_HTC_signal_20GeV_1e-4.sh
+++ xgboost/new_variables_training/20GEV_filter/RunAnSt1_HTC_signal_20GeV_1e-4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc11-opt/setup.sh
-python3 /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/xgboost/new_variables_training/training_macro.py --label "signal_20GeV_1e-4"
+python3 /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/xgboost/new_variables_training/20GEV_filter/training_macro.py --label "signal_20GeV_1e-4"
