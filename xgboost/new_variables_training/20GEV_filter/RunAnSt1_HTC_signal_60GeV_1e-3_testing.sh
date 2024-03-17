92874e6af4d852bbb08af869987aadae5b91d878
Thomas Matthew Critchley <tcritchl@lxplus731.cern.ch>
2024-03-17 21:54:03 +0100

    results run 9

---
+++ xgboost/new_variables_training/20GEV_filter/RunAnSt1_HTC_signal_60GeV_1e-3_testing.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc11-opt/setup.sh
+python3 /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/xgboost/new_variables_training/20GEV_filter/testing_macro.py --label "signal_60GeV_1e-3"
