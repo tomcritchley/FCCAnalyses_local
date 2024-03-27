69eb7fe23f19b4d1eac0d3ed2bc9d19a7e646e17
Thomas Matthew Critchley <tcritchl@lxplus731.cern.ch>
2024-03-27 10:46:30 +0100

    results

---
+++ DNN/DNN_condor/condor_outs_test1/RunAnSt1_HTC_50GeV_1e-3_preprocess.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/DNN/venv/bin/activate
+python3 /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/DNN/DNN_condor/preprocess_data.py --label "50GeV_1e-3"
