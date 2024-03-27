7ecaad8492846aa1e797fd65b9561c32f71e4b30
Thomas <thomas.critchley@etu.unige.ch>
2024-03-27 17:05:52 +0100

    testing phi

--- MC_hist.py
+++ MC_hist.py
@@ -49,8 +49,8 @@ signal_files = [
     ("/eos/user/t/tcritchl/MCfilter/HNL_Dirac_ejj_70GeV_1e-3Ve/chunk_0.root", "HNL_70GeV", ROOT.kOrange)
 ]
 tree_name = "events"
-variable_names = ("RecoElectron_lead_eta", "FSGenElectron_eta")
-hist_params = ("hist", "Histogram of RecoElectron_lead_eta and FSGenElectron_eta;eta;Events", 50, -2.5, 2.5)
+variable_names = ("RecoElectron_phi", "FSGenElectron_phi")
+hist_params = ("hist", "Histogram of RecoElectron_phi and FSGenElectron_phi;eta;Events", 50, -2.5, 2.5)
 
 # Create histograms for each file and variable
 histograms = []
