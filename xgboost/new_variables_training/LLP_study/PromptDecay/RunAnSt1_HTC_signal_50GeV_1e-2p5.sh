e3771a9adc293016dcec3f24fefcb8cc4a3f4d31
Thomas Matthew Critchley <tcritchl@lxplus704.cern.ch>
2024-04-04 14:37:22 +0200

    changes / clean up

---
+++ xgboost/new_variables_training/LLP_study/PromptDecay/RunAnSt1_HTC_signal_50GeV_1e-2p5.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /cvmfs/sft.cern.ch/lcg/views/dev3/Mon/x86_64-centos7-gcc11-opt/setup.sh
+python3 /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/xgboost/new_variables_training/LLP_study/PromptDecay/training_macro.py --label "signal_50GeV_1e-2p5"
