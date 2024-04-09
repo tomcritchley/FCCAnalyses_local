b75309e2a364ae77110936736a706bee70ecd63d
Thomas <thomas.critchley@etu.unige.ch>
2024-04-09 11:46:42 +0200

    gen vs reco eta

--- MC_hist.py
+++ MC_hist.py
@@ -38,8 +38,8 @@ signal_files = [
 ]
 
 tree_name = "events"
-variable_names = ("FSGenElectron_e", "RecoElectron_e")
-hist_params = ("theta", "Theta distribution;Theta (rad);Events", 50, 0, 50)
+variable_names = ("FSGenElectron_eta", "RecoElectron_eta")
+hist_params = ("eta", "Eta distribution;Eta (rad);Events", 50, -ROOT.TMath.Pi(), ROOT.TMath.Pi())
 
 histograms = []
 for file_path, label, color in background_files + signal_files:
@@ -79,4 +79,4 @@ text_lumi.SetTextSize(0.03)
 text_lumi.SetTextFont(42)
 text_lumi.DrawLatexNDC(0.70, 0.72, "#font[52]{#sqrt{s} = 91 GeV , #int L dt = 10 fb^{-1}}")
 
-c.SaveAs("comparison_plot_variables_energy.pdf")
+c.SaveAs("comparison_plot_variables_eta.pdf")
