84abaac1575eb09bf60f061f680634a8a3f34807
Thomas <thomas.critchley@etu.unige.ch>
2024-04-15 14:04:55 +0200

    4body

--- plots_4body/4body_truth_reco.py
+++ plots_4body/4body_truth_reco.py
@@ -31,8 +31,8 @@ label = "4body"
 color = ROOT.kBlue
 
 tree_name = "events"
-variable_names = ("FSGenElectron_px", "RecoElectron_px")
-hist_params = ("px", "px distribution;px;Events", 50, -50, 50)
+variable_names = ("FSGenElectron_e", "RecoElectron_e")
+hist_params = ("energy", "energy distribution;px;Events", 50, 0, 50)
 
 # Create histograms for Zbb truth and reco
 hist1, hist2 = create_histogram(file_path, tree_name, variable_names, hist_params, label, color)
@@ -54,4 +54,4 @@ text_title.SetTextSize(0.04)
 text_title.SetTextFont(42)
 text_title.DrawLatexNDC(0.1, 0.92, "#font[72]{FCCee} Simulation (DELPHES)")
 
-c.SaveAs("4body_px_comparison.pdf")
+c.SaveAs("4body_electron_e_comparison.pdf")
