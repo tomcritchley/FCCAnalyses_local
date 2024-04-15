c4aeb3894f4a1491f6c8985de8e2dc9ec32eda38
Thomas <thomas.critchley@etu.unige.ch>
2024-04-15 16:25:58 +0200

    at least 1 electron

--- plots_4body/4body_truth_reco.py
+++ plots_4body/4body_truth_reco.py
@@ -1,6 +1,6 @@
 import ROOT
 
-def create_histogram(file_path, tree_name, variable_names, hist_params, label, color):
+def create_histogram(file_path, tree_name, variable_names, hist_params, label, color, color2):
     # Creating histograms with more descriptive names
     hist1 = ROOT.TH1F(f"{hist_params[0]}_{label}_truth", f"Truth: {label}", hist_params[2], hist_params[3], hist_params[4])
     hist2 = ROOT.TH1F(f"{hist_params[0]}_{label}_reco", f"Reco: {label}", hist_params[2], hist_params[3], hist_params[4])
@@ -8,40 +8,36 @@ def create_histogram(file_path, tree_name, variable_names, hist_params, label, c
     f = ROOT.TFile.Open(file_path)
     tree = f.Get(tree_name)
     for event in tree:
-        value1_attr = getattr(event, variable_names[0], None)
-        value1 = value1_attr[0] if value1_attr.size() > 0 else float('nan')
-        value2_attr = getattr(event, variable_names[1], None)
-        value2 = value2_attr[0] if value2_attr.size() > 0 else float('nan')
-        
-        if not ROOT.TMath.IsNaN(value1): hist1.Fill(value1)
-        if not ROOT.TMath.IsNaN(value2): hist2.Fill(value2)
-
-        if not ROOT.TMath.IsNaN(value1) and value1 != 4:
-            hist1.Fill(value1)
-        if not ROOT.TMath.IsNaN(value2) and value2 != 4:
-            hist2.Fill(value2)
+        if event.n_FSGenElectron > 0:  # Ensuring at least one generated electron
+            value1_attr = getattr(event, variable_names[0], None)
+            value1 = value1_attr[0] if value1_attr.size() > 0 else float('nan')
+            value2_attr = getattr(event, variable_names[1], None)
+            value2 = value2_attr[0] if value2_attr.size() > 0 else float('nan')
+            
+            if not ROOT.TMath.IsNaN(value1): hist1.Fill(value1)
+            if not ROOT.TMath.IsNaN(value2): hist2.Fill(value2)
 
     f.Close()
 
     hist1.SetLineColor(color)
     hist1.SetStats(0)
     hist2.SetLineColor(color2)
-    hist2.SetLineStyle(0)  # Dashed line for reconstructed data
+    hist2.SetLineStyle(1)  # Solid line for reconstructed data
     hist2.SetStats(0)
     return hist1, hist2
 
 # File and parameters for Zbb
 file_path = "/eos/user/t/tcritchl/MCfilter/p8_ee_Zbb_ecm91/chunk_0.root"
 label = "z->bb"
-color = ROOT.kBlue
-color2 = ROOT.kRed
+color = ROOT.kBlue  # Color for truth data
+color2 = ROOT.kRed  # Color for reco data
 
 tree_name = "events"
 variable_names = ("FSGenElectron_e", "RecoElectron_e")
 hist_params = ("pt", "pt distribution;pt;Events", 100, 0, 50)
 
 # Create histograms for Zbb truth and reco
-hist1, hist2 = create_histogram(file_path, tree_name, variable_names, hist_params, label, color)
+hist1, hist2 = create_histogram(file_path, tree_name, variable_names, hist_params, label, color, color2)
 
 # Create canvas and draw histograms
 c = ROOT.TCanvas("c", "canvas", 1200, 800)
