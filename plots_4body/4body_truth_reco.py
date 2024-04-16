41a50b15e60c823cd48c0719cb6b78f75fca3a3e
Thomas <thomas.critchley@etu.unige.ch>
2024-04-16 10:33:41 +0200

    new plot

--- plots_4body/4body_truth_reco.py
+++ plots_4body/4body_truth_reco.py
@@ -2,7 +2,7 @@ import ROOT
 
 def create_histogram(file_path, tree_name, variable_names, hist_params, label, color, color2):
     # Creating histograms with more descriptive names
-    hist1 = ROOT.TH1F(f"{hist_params[0]}_{label}_truth", f"Truth: {label}", hist_params[2], hist_params[3], hist_params[4])
+    hist1 = ROOT.TH1F(f"{hist_params[0]}_{label}_truth", f"{label}", hist_params[2], hist_params[3], hist_params[4])
     hist2 = ROOT.TH1F(f"{hist_params[0]}_{label}_reco", f"Reco: {label}", hist_params[2], hist_params[3], hist_params[4])
 
     f = ROOT.TFile.Open(file_path)
@@ -27,8 +27,8 @@ def create_histogram(file_path, tree_name, variable_names, hist_params, label, c
     return hist1, hist2
 
 # File and parameters for Zbb
-file_path = "/eos/user/t/tcritchl/MCfilter/ejjnu/chunk_0.root" #p8_ee_Zbb_ecm91
-label = "z->4body"
+file_path = "/eos/user/t/tcritchl/MCfilter/p8_ee_Zbb_ecm91/chunk_0.root" #p8_ee_Zbb_ecm91
+label = "z-->bb"
 color = ROOT.kBlue  # Color for truth data
 color2 = ROOT.kRed  # Color for reco data
 
@@ -46,8 +46,8 @@ hist2.Draw("HISTSAME")
 
 # Adding a legend
 legend = ROOT.TLegend(0.7, 0.7, 0.9, 0.9)
-legend.AddEntry(hist1, "Truth: Z->4body", "l")
-legend.AddEntry(hist2, "Reco: Z->4body", "l")
+legend.AddEntry(hist1, "Truth: Z->bb", "l")
+legend.AddEntry(hist2, "Reco: Z->bb", "l")
 legend.Draw()
 
 # Adding text labels
@@ -56,4 +56,4 @@ text_title.SetTextSize(0.04)
 text_title.SetTextFont(42)
 text_title.DrawLatexNDC(0.1, 0.92, "#font[72]{FCCee} Simulation (DELPHES)")
 
-c.SaveAs(f"/afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/generator_plots/4body_electron_e.pdf")
+c.SaveAs(f"/afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/generator_plots/bb_electron_e.pdf")
