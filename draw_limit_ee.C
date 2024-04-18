fd15e4c856022f5dd483ff93619d99156df017d7
Thomas <thomas.critchley@etu.unige.ch>
2024-04-18 16:15:00 +0200

    aesthetics

--- draw_limit_ee.C
+++ draw_limit_ee.C
@@ -646,18 +646,21 @@ void draw_limit_ee() {
     // Use a single TLatex object to combine both parts of the text
 
     // Adjusted position and alignment of the combined text
-    //TLatex * tex = new TLatex(0.65, 0.85, "#it{#bf{FCCee}} Simulation (DELPHES)");  // Adjust x to move left, y to move down
-    //tex->SetNDC();
-    //tex->SetTextAlign(33);  // Right-align and vertically top-align
-    //tex->SetTextFont(42);
-    //tex->SetTextSize(0.04);
-    //tex->SetLineWidth(2);
-    //tex->Draw();
+    TLatex * tex = new TLatex(0.68, 0.83, "#it{#bf{FCCee}} Simulation (DELPHES)");  // Adjust x to move left, y to move down
+    tex->SetNDC();
+    tex->SetTextAlign(33);  // Right-align and vertically top-align
+    tex->SetTextFont(42);
+    tex->SetTextSize(0.04);
+    tex->SetLineWidth(2);
+    tex->Draw();
+
+    canvas.Update();
+    
+    Toms_data_gr.GetXaxis()->SetLabelOffset(0.02); // Increase the offset value slightly if it's still too close
 
     // Update the canvas
     canvas.Update();
 
-    // Run the application
     canvas.SaveAs("LimitSummary_ee_cms_bdt.pdf");
 
 }
