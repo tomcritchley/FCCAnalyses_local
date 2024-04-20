d881406bcb082b765606e0ac04e2cd8a8fe25c69
Thomas <thomas.critchley@etu.unige.ch>
2024-04-20 17:42:43 +0200

    30 gev and more events

--- DNN/DNN_condor/Data_Preparation.py
+++ DNN/DNN_condor/Data_Preparation.py
@@ -65,7 +65,7 @@ def load_and_preprocess_bkg(filepaths_and_xsecs, filter_func, label):
 def basic_filter(df):
     return df[
         (df["n_RecoElectrons"] == 1) & 
-        (df["RecoElectron_lead_e"] > 35) &
+        (df["RecoElectron_lead_e"] > 30) &
         (df["RecoDiJet_angle"] < np.pi) & 
         (df["RecoElectron_DiJet_delta_R"] < 5) &
         (df["RecoDiJet_phi"] < np.pi) & 
@@ -89,7 +89,7 @@ def prepare_datasets():
     signal_df['label'] = 1
 
     #background_files = [(os.path.join(dir, file), x_sec) for dir, x_sec in background_dirs for file in os.listdir(dir) if file.endswith('102.root') or file.endswith('ejjnu.root')]
-    background_files = [(os.path.join(dir, file), x_sec) for dir, x_sec in background_dirs for file in os.listdir(dir) if file.endswith('102.root') or file.endswith('ejjnu.root')]
+    background_files = [(os.path.join(dir, file), x_sec) for dir, x_sec in background_dirs for file in os.listdir(dir) if file.endswith('00.root') or file.endswith('ejjnu.root')]
     background_df = load_and_preprocess_bkg(background_files, basic_filter, 0)
 
     #Balancing the datasets
@@ -97,7 +97,7 @@ def prepare_datasets():
     n_background = len(background_df)
 
     #50/50 split for training and testing the signal
-    df_train_signal, df_test_signal = train_test_split(signal_df, test_size=0.5, random_state=42)
+    df_train_signal, df_test_signal = train_test_split(signal_df, test_size=0.2, random_state=42)
     
     #downsampling for the background to reduce class inequality 
     df_train_background = background_df.sample(n=min(n_signal, n_background // 2), random_state=42)
