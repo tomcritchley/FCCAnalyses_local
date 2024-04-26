99f2118698bb1e6be3db05dab292dca5908d2d83
Thomas <thomas.critchley@etu.unige.ch>
2024-04-26 15:21:31 +0200

    test 2

--- DNN/DNN_condor/Data_Preparation.py
+++ DNN/DNN_condor/Data_Preparation.py
@@ -98,26 +98,25 @@ def prepare_datasets():
 
 
     #to do 80/20 train test for both signal and background
-    """
-    df_train_background, df_test_background = train_test_split(background_df, test_size=0.67, random_state=42)
+    df_train_background, df_test_background = train_test_split(background_df, test_size=0.2, random_state=42)
 
     #80/20 split for training and testing the signal
     df_train_signal, df_test_signal = train_test_split(signal_df, test_size=0.2, random_state=42)
-    """
+
     #downsampling for the background to reduce class inequality 
     """
     df_train_background = background_df.sample(n=min(n_signal, n_background // 2), random_state=42)
     df_test_background = background_df.drop(df_train_background.index)
     """
 
-    #df_train = pd.concat([df_train_signal, df_train_background], ignore_index=True)
+    df_train = pd.concat([df_train_signal, df_train_background], ignore_index=True)
 
-    #df_test = pd.concat([df_test_signal, df_test_background], ignore_index=True)
-
+    df_test = pd.concat([df_test_signal, df_test_background], ignore_index=True)
+    """
     combined_df = pd.concat([signal_df, background_df])
 
     df_train, df_test = train_test_split(combined_df, test_size=0.2, random_state=22) #stratify=combined_df['label']
-
+    """
     # Separate back out into signal and background
     df_train_signal = df_train[df_train['label'] == 1]
     df_train_background = df_train[df_train['label'] == 0]
