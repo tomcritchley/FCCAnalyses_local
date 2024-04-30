d1ba6d4e70d1b8368c1a590228b7a998c36d55e7
Thomas Matthew Critchley <tcritchl@lxplus953.cern.ch>
2024-04-30 16:17:52 +0200

    changes

--- DNN/DNN_condor/DNN_less_bkg/Run6_opt/RunAnSt1_HTC_50GeV_1e-5.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/DNN/venv/bin/activate
-python3.11 /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/DNN/DNN_condor/Run6_opt/hyperparameter.py --label "50GeV_1e-5"
--- DNN/DNN_condor/DNN_less_bkg/Run6_opt/RunAnSt1_HTC_70GeV_1e-4p5.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/DNN/venv/bin/activate
-python3.11 /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/DNN/DNN_condor/Run6_opt/hyperparameter.py --label "70GeV_1e-4p5"
