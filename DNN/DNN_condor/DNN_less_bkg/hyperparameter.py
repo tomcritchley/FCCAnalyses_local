028cb26491b01165a31b61307a24e302d6f2f164
Thomas <thomas.critchley@etu.unige.ch>
2024-05-14 14:13:34 +0200

    run12 optimised

--- DNN/DNN_condor/DNN_less_bkg/hyperparameter.py
+++ DNN/DNN_condor/DNN_less_bkg/hyperparameter.py
@@ -14,12 +14,12 @@ def build_model(hp):
     model = Sequential()
     model.add(Dense(units=hp.Int('units_input', min_value=64, max_value=512, step=32),
                     input_dim=X_train.shape[1], activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2))
     
-    for i in range(hp.Int('num_layers', 3, 5)):
-        model.add(Dense(units=hp.Int(f'units_{i}', min_value=64, max_value=512, step=32),
+    for i in range(hp.Int('num_layers', 1, 3)):
+        model.add(Dense(units=hp.Int(f'units_{i}', min_value=32, max_value=512, step=32),
                         activation='relu'))
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.2))
     
     model.add(Dense(1, activation='sigmoid'))
     
@@ -41,49 +41,49 @@ def plot_metrics(history,label):
     plt.plot(history.history['val_prc'], label='Validation PR')
     plt.title('Precision-Recall Curve')
     plt.legend()
-    plt.savefig(f"/eos/user/t/tcritchl/DNN/DNN_plots10_opt/prec_recall_{label}.pdf")
-
-def permutation_feature_importance(model, X, y, feature_names,label):
-    from sklearn.metrics import accuracy_score
-    original_score = accuracy_score(y, model.predict(X).round())
-    feature_importance = np.zeros(X.shape[1])
-    
-    for i in range(X.shape[1]):
-        X_permuted = X.copy()
-        np.random.shuffle(X_permuted[:, i])
+    plt.savefig(f"/eos/user/t/tcritchl/DNN/DNN_plots12_opt/prec_recall_{label}.pdf")
+    """
+    def permutation_feature_importance(model, X, y, feature_names,label):
+        from sklearn.metrics import accuracy_score
+        original_score = accuracy_score(y, model.predict(X).round())
+        feature_importance = np.zeros(X.shape[1])
         
-        permuted_score = accuracy_score(y, model.predict(X_permuted).round())
-        feature_importance[i] = original_score - permuted_score
+        for i in range(X.shape[1]):
+            X_permuted = X.copy()
+            np.random.shuffle(X_permuted[:, i])
+            
+            permuted_score = accuracy_score(y, model.predict(X_permuted).round())
+            feature_importance[i] = original_score - permuted_score
 
-    plt.figure(figsize=(10, 5))
-    plt.bar(feature_names, feature_importance, color='lightblue')
-    plt.xlabel('Features')
-    plt.ylabel('Importance')
-    plt.xticks(rotation=45, ha="right")
-    plt.title('Permutation Feature Importance')
-    plt.tight_layout()
-    plt.savefig(f"/eos/user/t/tcritchl/DNN/DNN_plots10_opt/feature_importance_{label}.pdf")
-
+        plt.figure(figsize=(10, 5))
+        plt.bar(feature_names, feature_importance, color='lightblue')
+        plt.xlabel('Features')
+        plt.ylabel('Importance')
+        plt.xticks(rotation=45, ha="right")
+        plt.title('Permutation Feature Importance')
+        plt.tight_layout()
+        plt.savefig(f"/eos/user/t/tcritchl/DNN/DNN_plots10_opt/feature_importance_{label}.pdf")
+    """
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='DNN Training Script')
     parser.add_argument('--label', help='Label for the data', metavar='label')
     args = parser.parse_args()
 
     file = args.label
-    X_train = np.load(f'/eos/user/t/tcritchl/DNN/training10/X_train_{file}.npy', allow_pickle=True)
-    y_train = np.load(f'/eos/user/t/tcritchl/DNN/training10/y_train_{file}.npy', allow_pickle=True)
-    X_test = np.load(f'/eos/user/t/tcritchl/DNN/testing10/X_test_{file}.npy', allow_pickle=True)
-    y_test = np.load(f'/eos/user/t/tcritchl/DNN/testing10/y_test_{file}.npy', allow_pickle=True)
-    weights_train = np.load(f'/eos/user/t/tcritchl/DNN/testing10/weights_train_{file}.npy', allow_pickle=True)
+    X_train = np.load(f'/eos/user/t/tcritchl/DNN/training11/X_train_{file}.npy', allow_pickle=True)
+    y_train = np.load(f'/eos/user/t/tcritchl/DNN/training11/y_train_{file}.npy', allow_pickle=True)
+    X_test = np.load(f'/eos/user/t/tcritchl/DNN/testing11/X_test_{file}.npy', allow_pickle=True)
+    y_test = np.load(f'/eos/user/t/tcritchl/DNN/testing11/y_test_{file}.npy', allow_pickle=True)
+    weights_train = np.load(f'/eos/user/t/tcritchl/DNN/testing11/weights_train_{file}.npy', allow_pickle=True)
     
     feature_names = [
         r'$\Delta R_{jj}$', r'$\Delta R_{ejj}$', r'$D_0$', r'Dijet $\phi$', r'E_{\text{miss}} $\theta$',
         r'$E_{\text{miss}}$', r'$E_{e}$', r'Vertex $\chi^2$', r'$n_{\text{Primary Tracks}}$', r'$n_{\text{Tracks}}$'
     ]
     
-    tuner = RandomSearch(build_model, objective=Objective("val_prc", direction="max"), max_trials=50,
-                         executions_per_trial=1, directory='model_tuning', project_name='tuning_results')
-    callbacks = [EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)]
+    tuner = RandomSearch(build_model, objective=Objective("val_prc", direction="max"), max_trials=25,
+                         executions_per_trial=1, directory='model_tuning', project_name=f'tuning_{file}_results')
+    callbacks = [EarlyStopping(monitor='val_loss', patience=40, restore_best_weights=True)]
 
 
     val_start_index = int(len(y_train) * (1 - 0.3))
@@ -93,13 +93,12 @@ if __name__ == "__main__":
     print(f'Average class probability in validation set: {y_val.mean():.4f}')
     print(f'Average class probability in test set:       {y_test.mean():.4f}')
 
-    tuner.search(X_train, y_train, epochs=50, validation_split=0.2, callbacks=callbacks)
+    tuner.search(X_train, y_train, epochs=60, validation_split=0.2, callbacks=callbacks)
 
     best_model = tuner.get_best_models(num_models=1)[0]
-    best_model.save(f'/eos/user/t/tcritchl/DNN/trained_models10_opt/DNN_HNLs_{args.label}.keras')
+    best_model.save(f'/eos/user/t/tcritchl/DNN/trained_models12_opt/DNN_HNLs_{args.label}.keras')
     print("Best model saved successfully.")
 
-    history = best_model.fit(X_train, y_train, sample_weight=weights_train, epochs=100, validation_split=0.2, callbacks=[EarlyStopping(monitor='val_loss', patience=5)])
+    history = best_model.fit(X_train, y_train, epochs=60, validation_split=0.2, callbacks=[EarlyStopping(monitor='val_loss', patience=40)]) # sample_weight=weights_train
     label = file
     plot_metrics(history,label)
-    permutation_feature_importance(best_model, X_test, y_test, feature_names,label)
