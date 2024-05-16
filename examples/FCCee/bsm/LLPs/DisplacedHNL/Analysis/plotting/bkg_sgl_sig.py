43e398a82c8cd4c0178a7d93c464b2a028c33051
Thomas <thomas.critchley@etu.unige.ch>
2024-05-16 15:16:38 +0200

    aesthetics

--- examples/FCCee/bsm/LLPs/DisplacedHNL/Analysis/plotting/bkg_sgl_sig.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/Analysis/plotting/bkg_sgl_sig.py
@@ -26,7 +26,7 @@ variable_list = [
     ["Vertex_chi2", "Chi^{2} of the primary vertex"], #16
 ]
 
-chosen_variable = variable_list[9] 
+chosen_variable = variable_list[5] 
 
 significance_directions = ["LR", "RL"]
 significance_direction = significance_directions[0]
