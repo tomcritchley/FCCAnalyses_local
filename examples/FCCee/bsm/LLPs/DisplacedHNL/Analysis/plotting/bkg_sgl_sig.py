951a67835e5d585bb54c87a10c4c4d72b68567d8
Thomas <thomas.critchley@etu.unige.ch>
2024-05-16 10:47:35 +0200

    positioning

--- examples/FCCee/bsm/LLPs/DisplacedHNL/Analysis/plotting/bkg_sgl_sig.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/Analysis/plotting/bkg_sgl_sig.py
@@ -192,12 +192,12 @@ def make_plot(h_list_signal, h_list_bg, legend_list_signal, legend_list_bg, h_li
     pad2.Draw()
     
     # Background Legend
-    leg_bg = ROOT.TLegend(0.60, 0.55, 0.80, 0.70)
+    leg_bg = ROOT.TLegend(0.70, 0.55, 0.90, 0.70)
     leg_bg.SetFillStyle(0)
     leg_bg.SetLineWidth(0)
 
     # Signal Legend
-    leg_sig = ROOT.TLegend(0.60, 0.35, 0.80, 0.50)
+    leg_sig = ROOT.TLegend(0.70, 0.35, 0.90, 0.50)
     leg_sig.SetFillStyle(0)
     leg_sig.SetLineWidth(0)
 
@@ -267,21 +267,20 @@ def make_plot(h_list_signal, h_list_bg, legend_list_signal, legend_list_bg, h_li
     leg_sig.Draw()
     leg_bg.Draw()
 
-    ### Top left text elements ###
     text_title = ROOT.TLatex()
     text_title.SetTextSize(0.04)
     text_title.SetTextFont(42)
-    text_title.DrawLatexNDC(0.15, 0.92, "#font[72]{FCCee} Simulation (DELPHES)")
+    text_title.DrawLatexNDC(0.10, 0.92, "#font[72]{FCCee} Simulation (DELPHES)")
 
     text_selection = ROOT.TLatex()
     text_selection.SetTextSize(0.03)
     text_selection.SetTextFont(42)
-    text_selection.DrawLatexNDC(0.15, 0.87, "#font[52]{No Selection}")
+    text_selection.DrawLatexNDC(0.10, 0.84, "#font[52]{No Selection}")
 
     text_lumi = ROOT.TLatex()
     text_lumi.SetTextSize(0.03)
     text_lumi.SetTextFont(42)
-    text_lumi.DrawLatexNDC(0.15, 0.82, "#font[52]{#sqrt{s} = 91 GeV , #int L dt = 10 fb^{-1}}")
+    text_lumi.DrawLatexNDC(0.10, 0.79, "#font[52]{#sqrt{s} = 91 GeV , #int L dt = 10 fb^{-1}}")
 
     pad1.RedrawAxis()
 
