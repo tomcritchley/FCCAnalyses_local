c46c8c0e902a2856f8254b4a82626301fe8c6399
Thomas Matthew Critchley <tcritchl@lxplus999.cern.ch>
2024-05-16 10:38:10 +0200

    changes'

---
+++ DNN/Run15/RunAnSt1_HTC_80GeV_1e-5_preprocess.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/DNN/venv/bin/activate
+python3.11 /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/DNN/Run15/Data_Preparation.py --label "80GeV_1e-5"
---
+++ DNN/Run16/RunAnSt1_HTC_80GeV_1e-4p5_preprocess.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/DNN/venv/bin/activate
+python3.11 /afs/cern.ch/work/t/tcritchl/FCCAnalyses_local/DNN/Run16/Data_Preparation.py --label "80GeV_1e-4p5"
