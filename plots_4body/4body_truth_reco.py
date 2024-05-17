75bc4afb0861f5c6fb91704336dad544388d1fa8
Thomas <thomas.critchley@etu.unige.ch>
2024-05-17 11:17:39 +0200

    fix

--- plots_4body/4body_truth_reco.py
+++ plots_4body/4body_truth_reco.py
@@ -14,7 +14,7 @@ def create_histogram(file_path, tree_name, variable_names, hist_params, label, c
                 energy = event.FSGenElectron_e[i]
                 pt = event.FSGenElectron_pt[i]
                 # Apply the DELPHES efficiency conditions
-                if energy > 2.0 and pt >= 0.1 and eta <= 2.56: #--> the DELPHES condition
+                if energy >= 2.0 and pt >= 0.1 and eta <= 2.56: #--> the DELPHES condition
                     value1_attr = getattr(event, variable_names[0], None)
                     value1 = value1_attr[0] if value1_attr.size() > 0 else float('nan')
                     value2_attr = getattr(event, variable_names[1], None)
@@ -41,7 +41,7 @@ color2 = ROOT.kRed  # Color for reco data
 
 tree_name = "events"
 variable_names = ("FSGenElectron_e", "RecoElectron_e")
-hist_params = ("pt", "Energy distribution;Eta;Events", 100, 0, 50)  # Updated axis labels and range
+hist_params = ("pt", "Energy distribution;Eta;Events", 100, 2, 50)  # Updated axis labels and range
 #hist_params = ("pt", "pt distribution;pt;Events", 100, -ROOT.TMath.Pi(), ROOT.TMath.Pi())
 
 # Create histograms for Zbb truth and reco
