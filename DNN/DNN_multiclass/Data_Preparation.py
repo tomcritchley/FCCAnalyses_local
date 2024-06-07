a18dacc68f873aaf9f8c60723a4e409158fe1b6a
Thomas <thomas.critchley@etu.unige.ch>
2024-06-07 12:39:21 +0200

    testing with real weights

--- DNN/DNN_multiclass/Data_Preparation.py
+++ DNN/DNN_multiclass/Data_Preparation.py
@@ -37,8 +37,12 @@ background_dirs = [
 def load_and_filter_data(filepath, x_sec, tree_name, variables, filter_func):
     with uproot.open(filepath) as file:
         df = file[tree_name].arrays(variables, library="pd")
-        df['weight'] = (float(x_sec) * target_luminosity) / len(df)
-        return filter_func(df)
+        n_events_before_filter = len(df)
+        df = filter_func(df)
+        n_events_after_filter = len(df)
+        selection_efficiency = n_events_after_filter / n_events_before_filter
+        df['weight'] = ((float(x_sec) * target_luminosity) / n_events_before_filter) * selection_efficiency
+        return df, n_events_before_filter, n_events_after_filter
 
 # Chunks of background with a retry mechanism
 def load_and_preprocess_bkg(filepath, x_sec, filter_func, label):
@@ -46,30 +50,38 @@ def load_and_preprocess_bkg(filepath, x_sec, filter_func, label):
     attempt = 0
     max_attempts = 3
     success = False
+    n_events_before_filter_total = 0
+    n_events_after_filter_total = 0
     
     while attempt < max_attempts and not success:
         try:
             with uproot.open(f"{filepath}:{tree_name}") as tree:
                 df = tree.arrays(variables, library="pd")
-                df['cross_section'] = float(x_sec)
+                n_events_before_filter = len(df)
                 df = filter_func(df)
+                n_events_after_filter = len(df)
+                selection_efficiency = n_events_after_filter / n_events_before_filter
+                
                 if x_sec == "5215.46":
-                    df['weight'] = (df['cross_section'] * target_luminosity) / 498091935
+                    df['weight'] = ((float(x_sec) * target_luminosity) / 498091935) * selection_efficiency
                 elif x_sec == "6654.46":
-                    df['weight'] = (df['cross_section'] * target_luminosity) / 438538637
+                    df['weight'] = ((float(x_sec) * target_luminosity) / 438538637) * selection_efficiency
                 elif x_sec == "0.014":
-                    df['weight'] = (df['cross_section'] * target_luminosity) / 100000
+                    df['weight'] = ((float(x_sec) * target_luminosity) / 100000) * selection_efficiency
+
                 df['label'] = label
                 dfs.append(df)
                 success = True
+                n_events_before_filter_total += n_events_before_filter
+                n_events_after_filter_total += n_events_after_filter
         except Exception as e:
             attempt += 1
             print(f"Error processing file {filepath} on attempt {attempt}: {e}")
             if attempt < max_attempts:
-                time.sleep(2)  # wait for 2 seconds before retrying
+                time.sleep(2) 
             else:
                 print(f"Failed to process file {filepath} after {max_attempts} attempts.")
-    return pd.concat(dfs, ignore_index=True)
+    return pd.concat(dfs, ignore_index=True), n_events_before_filter_total, n_events_after_filter_total
 
 def basic_filter(df):
     return df[
@@ -93,16 +105,28 @@ def prepare_datasets():
 
     signal_file = f"{base_HNL}/HNL_Dirac_ejj_{args.label}Ve.root"
     signal_x_sec = cross_section_dict[f"HNL_Dirac_ejj_{args.label}Ve"]['cross_section_pb']
-    signal_df = load_and_filter_data(signal_file, signal_x_sec, tree_name, variables, basic_filter)
+    signal_df, n_signal_before_filter, n_signal_after_filter = load_and_filter_data(signal_file, signal_x_sec, tree_name, variables, basic_filter)
     signal_df['label'] = 1
 
     training_sets = {}
+    background_selection_data = []
     for i, (background_dir, x_sec) in enumerate(background_dirs):
         print(f"Processing background type {i} with cross section {x_sec} pb")
 
         background_files = [os.path.join(background_dir, file) for file in os.listdir(background_dir) if file.endswith('00.root') or file.endswith('ejjnu.root')]
-        background_df = pd.concat([load_and_preprocess_bkg(filepath, x_sec, basic_filter, 0) for filepath in background_files], ignore_index=True)
-        
+        background_dfs = []
+        n_bkg_before_filter_total = 0
+        n_bkg_after_filter_total = 0
+
+        for filepath in background_files:
+            background_df, n_bkg_before_filter, n_bkg_after_filter = load_and_preprocess_bkg(filepath, x_sec, basic_filter, 0)
+            background_dfs.append(background_df)
+            n_bkg_before_filter_total += n_bkg_before_filter
+            n_bkg_after_filter_total += n_bkg_after_filter
+
+        background_df = pd.concat(background_dfs, ignore_index=True)
+        background_selection_data.append((n_bkg_before_filter_total, n_bkg_after_filter_total))
+
         signal_train, signal_test = train_test_split(signal_df, test_size=0.5, random_state=42)
         background_train, background_test = train_test_split(background_df, test_size=0.5, random_state=42)
 
@@ -124,7 +148,14 @@ def prepare_datasets():
         else:
             combined_test = pd.concat([combined_test, df_test], ignore_index=True)
 
-    # Convert to numpy arrays
+    signal_factor = len(signal_df) / len(signal_test)
+    combined_test.loc[combined_test['label'] == 1, 'weight'] *= signal_factor
+
+    for j, (background_dir, x_sec) in enumerate(background_dirs):
+        n_bkg_before_filter, n_bkg_after_filter = background_selection_data[j]
+        background_factor = len(background_df) / len(background_test)
+        combined_test.loc[(combined_test['label'] == 0) & (combined_test['cross_section'] == float(x_sec)), 'weight'] *= background_factor
+
     def convert_to_numpy(df, fields):
         for field in fields:
             df[field] = ak.to_numpy(df[field])
@@ -134,7 +165,6 @@ def prepare_datasets():
 
     convert_to_numpy(combined_test, variables)
 
-    # Standardize and save datasets
     scaler = StandardScaler()
 
     for key, df_train in training_sets.items():
